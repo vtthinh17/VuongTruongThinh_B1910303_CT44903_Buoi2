eb88f446ef8a3a3a77cb63d6c29cdf9c78550e4f
vtthinh <vtthinh17st@gmail.com>
2022-09-20 21:43:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
 
@@ -8,8 +9,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts",contactsRouter);
 
-app.get("/", (req,res) => {
-    res.json({message: "Welcome to contact book application."});
+//khi khong co URL nao khop voi cac route dinh nghia o contactsRouter
+//middle nay se xu ly loi 404 not found
+app.use((req, res, next) => {
+    return next(new ApiError(404,"Resource not found"));
+});
+//middleware xu ly loi khi cac route tren khong match
+app.use((err, req, res, next)=>{
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    });
 });
 
 module.exports = app;
